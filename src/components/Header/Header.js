82ed12c332a8f00241144d631e506c79d61d40f7
ngviet00 <ngvviet0306@gmail.com>
2021-05-30 19:20:57 +0700

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import { Link } from 'react-router-dom';
 import logo from './../../image/logo.svg';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import Button from '@material-ui/core/Button';
 
 function Header() {
+
+   const [toggle, setToggle] = useState(false);
+
+   const toggleMenu = () => {
+      setToggle(!toggle);
+   }
+
+   const [width, setWidth] = useState(0);
+   function reportWindowSize() {
+      setWidth(window.innerWidth);
+      if (width > 768) {
+         setToggle(false);
+      }
+   }
+
+   window.onresize = reportWindowSize;
+
    return (
       <div className="header">
          <div className="wp_container wrapper_container">
@@ -34,10 +53,42 @@ function Header() {
                <Link to="/SignUp" id="signUp" variant="contained" color="primary">
                   Sign Up
                </Link>
-               <MenuIcon id="menu__icon"></MenuIcon>
+               {toggle ?
+                  <CloseIcon onClick={toggleMenu} className="menu__icon" />
+                  : <MenuIcon onClick={toggleMenu} className="menu__icon" />}
+            </div>
+
+            <div id="toggle_menu" className={`${toggle ? "display_menu" : ""}`}>
+               <ul>
+                  <li>
+                     <Link to="">Solutions</Link>
+                  </li>
+                  <li>
+                     <Link to="">Team</Link>
+                  </li>
+                  <li>
+                     <Link to="">Price</Link>
+                  </li>
+                  <li>
+                     <Link to="">Feature</Link>
+                  </li>
+                  <li>
+                     <Link to="/SignIn">
+                        Sign In
+                     </Link>
+                  </li>
+
+                  <li>
+                     <Link to="/SignUp">
+                        <Button variant="contained" color="primary" disableElevation>
+                           Sign Up
+                        </Button>
+                     </Link>
+                  </li>
+               </ul>
             </div>
          </div>
-      </div>
+      </div >
    )
 }
 
