2e000ca75e6b746716f82caecad253b35d2e2136
ngviet00 <ngvviet0306@gmail.com>
2021-06-01 17:49:33 +0700

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Hero.css';
+import Button from '@material-ui/core/Button';
 function Hero() {
    return (
       <div className="hero">
-         <div id="mask">
-            <div id="started">
-               <h1>Jumpstart your next</h1>
-               <h2>SaaS product</h2>
-               <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Incidunt optio officiis dolore earum error eaque perferendis laudantium sed.
+         <div id="started">
+            <h1>Jumpstart your next</h1>
+            <h2>SaaS product</h2>
+            <p>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+               Incidunt optio officiis dolore earum error eaque perferendis laudantium sed.
                </p>
-               <div id="btn_started">
-                  <button id="btn__started">Get Started</button>
-                  <button id="btn__demo">Live Demo</button>
-               </div>
+            <div id="btn_started">
+               <Button variant="contained" color="primary">
+                  Get Started
+               </Button>
+               <Button variant="outlined" color="primary" id="btn_live_demo">
+                  Live Demo
+               </Button>
             </div>
-
          </div>
-      </div>
+      </div >
    )
 }
 export default Hero;
